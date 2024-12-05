98db79c18e5d3d6ffa6e5acdcba39c270df3aacf
Peter Caisse <pcaisse@gmail.com>
2024-12-04 22:25:38 -0500

    Another attempt in JS

--- day04/day04.js
+++ day04/day04.js
@@ -3,17 +3,45 @@ const input = require("fs").readFileSync(process.stdin.fd).toString();
 const w = input.indexOf("\n");
 const s = input.replaceAll("\n", "");
 
-const regex = new RegExp(
-  `(?=(SAMX|XMAS|S.{${w - 1}}A.{${w - 1}}M.{${w - 1}}X|S.{${w - 1}}A.{${
-    w - 1
-  }}M.{${w - 1}}X|X.{${w - 1}}M.{${w - 1}}A.{${w - 1}}S|X.{${w - 2}}M.{${
-    w - 2
-  }}A.{${w - 2}}S|X.{${w}}M.{${w}}A.{${w}}S|S.{${w}}A.{${w}}M.{${w}}X|S.{${
-    w - 2
-  }}A.{${w - 2}}M.{${w - 2}}X))`,
-  "g"
+const xmas = (s, xi, mi, ai, si) => ({
+  match: s[xi] === "X" && s[mi] === "M" && s[ai] === "A" && s[si] === "S",
+  indices: [xi, mi, ai, si],
+});
+
+const left = (s, i) => xmas(s, i, i - 1, i - 2, i - 3);
+const right = (s, i) => xmas(s, i, i + 1, i + 2, i + 3);
+const up = (s, i) => xmas(s, i, i - w, i - 2 * w, i - 3 * w);
+const upLeft = (s, i) => xmas(s, i, i - w - 1, i - 2 * w - 2, i - 3 * w - 3);
+const upRight = (s, i) => xmas(s, i, i - w + 1, i - 2 * w + 2, i - 3 * w + 3);
+const down = (s, i) => xmas(s, i, i + w, i + 2 * w, i + 3 * w);
+const downLeft = (s, i) => xmas(s, i, i + w - 1, i + 2 * w - 2, i + 3 * w - 3);
+const downRight = (s, i) => xmas(s, i, i + w + 1, i + 2 * w + 2, i + 3 * w + 3);
+
+const matchingIndices = new Set();
+let total = 0;
+
+[...s].forEach((c, index) =>
+  [left, right, up, upLeft, upRight, down, downLeft, downRight].forEach((f) => {
+    const { match, indices } = f(s, index);
+    if (match) {
+      for (const i of indices) {
+        matchingIndices.add(i);
+      }
+      total++;
+    }
+  })
 );
-console.log(regex);
-const total = Array.from(s.matchAll(regex)).length;
+
+let output = "";
+for (let i = 0; i < s.length; i++) {
+  if (matchingIndices.has(i)) {
+    output += s[i];
+  } else {
+    output += ".";
+  }
+  if ((i + 1) % w === 0) output += "\n";
+}
+
+console.log(output);
 
 console.log(total);
