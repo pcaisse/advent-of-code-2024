1e1347a9d83c2267e9e26751394a632d0c28b43b
Peter Caisse <pcaisse@gmail.com>
2024-12-06 19:31:16 -0500

    Day 6 part 2

--- day06/day06.js
+++ day06/day06.js
@@ -1,10 +1,11 @@
 const input = require("fs").readFileSync(process.stdin.fd).toString();
 const w = input.indexOf("\n") + 1;
 
-const start = {
-  index: input.indexOf("^"),
-  orientation: "UP",
-};
+const OBSTACLE = "#";
+const startIndex = input.indexOf("^");
+const obstacleIndices = [...input].filter((_, i, a) => a[i] === OBSTACLE);
+
+const prohibitedStartingIndices = new Set(obstacleIndices.concat(startIndex));
 
 const left = (x) => x - 1;
 const right = (x) => x + 1;
@@ -25,33 +26,43 @@ const nextOrientation = {
   LEFT: "UP",
 };
 
-function take_step({ index, orientation }) {
+function take_step({ index, orientation }, specialObstacleIndex) {
   const nextIndex = orientationToMovement[orientation](index);
-  if (input[nextIndex] === "#") {
-    console.log("hit an obstacle");
+  if (input[nextIndex] === OBSTACLE || nextIndex === specialObstacleIndex) {
     return { index, orientation: nextOrientation[orientation] };
   }
   return { index: nextIndex, orientation };
 }
 
-const positionsVisited = new Set([start.index]);
+const positionToString = (position) =>
+  `${position.index}${position.orientation}`;
 
-let currentPosition = start;
-
-while (true) {
-  console.log("currentPosition", currentPosition);
-  console.log("positionsVisited.size", positionsVisited.size);
-  const nextPosition = take_step(currentPosition);
-  if (
-    input[nextPosition.index] === "\n" ||
-    input[nextPosition.index] === undefined
-  )
-    break;
-  if (nextPosition.index !== currentPosition.index) {
-    console.log("increment positionsVisited");
-    positionsVisited.add(nextPosition.index);
+function walk(startingIndex, specialObstacleIndex) {
+  let currentPosition = {
+    index: startingIndex,
+    orientation: "UP",
+  };
+  const previousPositions = new Set();
+  while (true) {
+    const nextPosition = take_step(currentPosition, specialObstacleIndex);
+    if (
+      input[nextPosition.index] === "\n" ||
+      input[nextPosition.index] === undefined
+    )
+      return false;
+    if (previousPositions.has(positionToString(currentPosition))) {
+      return true;
+    }
+    previousPositions.add(positionToString(currentPosition));
+    currentPosition = nextPosition;
   }
-  currentPosition = nextPosition;
 }
 
-console.log(positionsVisited.size);
+const obstructionCount = [...input].filter((_, i) => {
+  if (prohibitedStartingIndices.has(i)) {
+    return false;
+  }
+  return walk(startIndex, i);
+}).length;
+
+console.log(obstructionCount);
