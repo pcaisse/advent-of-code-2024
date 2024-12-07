320e4670a7c841f515c3a66803934d2ae1be3ec3
Peter Caisse <pcaisse@gmail.com>
2024-12-07 09:40:32 -0500

    Day 7 part 1

---
+++ day07/day07.js
@@ -0,0 +1,34 @@
+const input = require("fs").readFileSync(process.stdin.fd).toString();
+
+const add = (x, y) => x + y;
+const multiply = (x, y) => x * y;
+
+const possibleResults = (terms) =>
+  terms.reduce((allPossibilities, x) => {
+    if (allPossibilities.length === 0) {
+      return [x];
+    } else {
+      return allPossibilities.flatMap((value) => [
+        add(value, x),
+        multiply(value, x),
+      ]);
+    }
+  }, []);
+
+const equations = input
+  .split("\n")
+  .filter((l) => l)
+  .map((s) => {
+    const [left, right] = s.split(":");
+    const result = Number(left);
+    const terms = right.trim().split(" ").map(Number);
+    return { result, terms };
+  })
+  .filter(({ result, terms }) => {
+    const allPossibleResults = new Set(possibleResults(terms));
+    return allPossibleResults.has(result);
+  })
+  .map(({ result }) => result)
+  .reduce(add, 0);
+
+console.log(equations);
