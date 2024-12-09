5f6a411a2ab2b09f82f95203ad7684ecd98c62c4
Peter Caisse <pcaisse@gmail.com>
2024-12-09 09:57:31 -0500

    Day 8 part 2

--- day08/day08.js
+++ day08/day08.js
@@ -10,39 +10,64 @@ let s2 = s;
 const serialize = (i, j) => `${i}-${j}`;
 const deserialize = (str) => str.split("-").map(Number);
 
-function newAntinodes(i, j, rowIndex, colIndex, numRows, numCols) {
-  let set = new Set(); //([serialize(i, j), serialize(rowIndex, colIndex)]);
+function newAntinodes(
+  i,
+  j,
+  rowIndex,
+  colIndex,
+  numRows,
+  numCols,
+  before = true,
+  after = true
+) {
+  let set = new Set([serialize(i, j), serialize(rowIndex, colIndex)]);
 
   const rowDiff = i - rowIndex;
   const colDiff = j - colIndex;
 
-  console.log("rowDiff", rowDiff, "colDiff", colDiff);
-
   const newRowIndex1 = rowIndex - rowDiff;
   const newColIndex1 = colIndex - colDiff;
-
-  console.log("newRowIndex1", newRowIndex1, "newColIndex1", newColIndex1);
 
   const rowIsValid = (val) => val >= 0 && val < numRows;
   const colIsValid = (val) => val >= 0 && val < numCols;
 
-  if (rowIsValid(newRowIndex1) && colIsValid(newColIndex1)) {
-    // console.log("newRowIndex1", newRowIndex1, "newColIndex1", newColIndex1);
-    set.add(serialize(newRowIndex1, newColIndex1));
-    // set = set.union(
-    //   newAntinodes(rowIndex, colIndex, newRowIndex1, newColIndex1)
-    // );
+  if (before) {
+    if (rowIsValid(newRowIndex1) && colIsValid(newColIndex1)) {
+      set.add(serialize(newRowIndex1, newColIndex1));
+      set = set.union(
+        newAntinodes(
+          rowIndex,
+          colIndex,
+          newRowIndex1,
+          newColIndex1,
+          numRows,
+          numCols,
+          true,
+          false
+        )
+      );
+    }
   }
 
   const newRowIndex2 = i + rowDiff;
   const newColIndex2 = j + colDiff;
 
-  if (rowIsValid(newRowIndex2) && colIsValid(newColIndex2)) {
-    // console.log("newRowIndex2", newRowIndex2, "newColIndex2", newColIndex2);
-    set.add(serialize(newRowIndex2, newColIndex2));
-    // set = set.union(
-    //   newAntinodes(rowIndex, colIndex, newRowIndex2, newColIndex2)
-    // );
+  if (after) {
+    if (rowIsValid(newRowIndex2) && colIsValid(newColIndex2)) {
+      set.add(serialize(newRowIndex2, newColIndex2));
+      set = set.union(
+        newAntinodes(
+          newRowIndex2,
+          newColIndex2,
+          i,
+          j,
+          numRows,
+          numCols,
+          false,
+          true
+        )
+      );
+    }
   }
 
   return set;
@@ -64,18 +89,6 @@ for (let i = 0; i < arr.length; i++) {
     } else {
       for (let k = 0; k < antennas[c].length; k++) {
         const [rowIndex, colIndex] = deserialize(antennas[c][k]);
-        console.log(
-          "c",
-          c,
-          "i",
-          i,
-          "j",
-          j,
-          "rowIndex",
-          rowIndex,
-          "colIndex",
-          colIndex
-        );
         const na = newAntinodes(
           i,
           j,
@@ -84,14 +97,14 @@ for (let i = 0; i < arr.length; i++) {
           arr.length,
           row.length
         );
-        console.log("na", na);
         na.forEach((antinodeStr) => {
           const [rowIndex, colIndex] = deserialize(antinodeStr);
           const antinodeIndex = rowIndex * (row.length + 1) + colIndex;
-          s2 =
-            s2.substring(0, antinodeIndex) +
-            "#" +
-            s2.substring(antinodeIndex + 1);
+          if (s2[antinodeIndex] === ".")
+            s2 =
+              s2.substring(0, antinodeIndex) +
+              "#" +
+              s2.substring(antinodeIndex + 1);
         });
         antinodes = antinodes.union(na);
       }
