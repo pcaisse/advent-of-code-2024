5c71823897abc84bab8d6cbd57d2f8aa6cb2d391
Peter Caisse <pcaisse@gmail.com>
2024-12-12 10:48:51 -0500

    Day 11 part 2

--- day11/day11.js
+++ day11/day11.js
@@ -2,25 +2,39 @@ const s = require("fs").readFileSync(process.stdin.fd).toString();
 
 const stones = s.split(" ").map((stone) => stone.trim());
 
-function blink(stones) {
-  return stones.flatMap((stone) =>
-    stone === "0"
-      ? "1"
-      : stone.length % 2 === 0
-      ? [
-          String(Number(stone.substring(0, stone.length / 2))),
-          String(Number(stone.substring(stone.length / 2))),
-        ]
-      : String(Number(stone) * 2024)
-  );
+function changeVal(stone) {
+  return stone === "0"
+    ? ["1"]
+    : stone.length % 2 === 0
+    ? [
+        String(Number(stone.substring(0, stone.length / 2))),
+        String(Number(stone.substring(stone.length / 2))),
+      ]
+    : [String(Number(stone) * 2024)];
 }
 
-function applyNTimes(func, arg, n) {
-  let ret = func(arg);
-  for (let i = 0; i < n - 1; i++) {
-    ret = func(ret);
+const howMany = {};
+
+function blink(stone, n, i) {
+  if (i === n) return 1;
+
+  const [val1, val2] = changeVal(stone);
+
+  let totalCount = 0;
+  if (val1) {
+    howMany[val1] = howMany[val1] || {};
+    const leftCount = howMany[val1][i] ?? blink(val1, n, i + 1);
+    howMany[val1][i] = leftCount;
+    totalCount += leftCount;
   }
-  return ret;
+  if (val2) {
+    howMany[val2] = howMany[val2] || {};
+    const rightCount = howMany[val2][i] ?? blink(val2, n, i + 1);
+    howMany[val2][i] = rightCount;
+    totalCount += rightCount;
+  }
+  return totalCount;
 }
 
-console.log(applyNTimes(blink, stones, 25).length);
+const ret = stones.map((stone) => blink(stone, 75, 0)).reduce((a, b) => a + b);
+console.log(ret);
